6f7b2a08742e4c502968aa10e4e92dfebb5da55a
Govind-Deshmukh <govinddeshmukh2001@gmail.com>
2022-10-08 15:22:07 +0530

    added more files related to project

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./components/Dashboard/Dashboard";
+
+import IndexPage from "./components/LoginRegisterComps/IndexPage";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
